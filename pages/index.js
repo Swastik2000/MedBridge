80537ee353d3b555ad20336b4094623cd0553cb0
Soumava-221B <77767745+Soumava-221B@users.noreply.github.com>
2023-06-04 10:21:28 +0000

    index and seller page updated

--- pages/index.js
+++ pages/index.js
@@ -37,18 +37,18 @@ export default function Index() {
           <div class="flex flex-col justify-center sm:text-center lg:py-12 lg:text-left xl:w-5/12 xl:py-24">
             {/* <p class="mb-4 font-semibold text-indigo-500 md:mb-6 md:text-lg xl:text-xl">Very proud to introduce</p> */}
     
-            <h1 class="text-black-800 mb-8 text-4xl font-bold sm:text-5xl md:mb-12 md:text-6xl">Accessible healthcare, anytime, anywhere</h1>
+            <h1 class="text-black-800 mb-8 text-4xl font-bold sm:text-5xl md:mb-12 md:text-6xl">Accessible medicines, anytime, anywhere</h1>
     
             <p class="mb-8 leading-relaxed text-gray-500 md:mb-12 lg:w-4/5 xl:text-lg">MedBridge is a project that aims to promote access to medicines by facilitating the sharing of surplus medicines.
 The idea behind the project is to reduce the waste of medications that are still safe and effective for use, while
-also addressing the issue of affordability and accessibility of medicines for those who need them the most.</p>
+also addressing the issue of accessibility of medicines for those who need them the most.</p>
     
             <div class="flex flex-col gap-2.5 sm:flex-row sm:justify-center lg:justify-start">
             <Link href="./buyerPage">
               <a  class="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base">Browse Medicines</a>
               </Link>
               <Link href="./sellerPage">
-              <a href="#" class="inline-block rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base">Selling?</a>
+              <a href="#" class="inline-block rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base">Want to sell?</a>
               </Link>
             </div>
           </div>
@@ -97,7 +97,7 @@ also addressing the issue of affordability and accessibility of medicines for th
     
             <div>
               <h3 class="mb-2 text-lg font-semibold md:text-xl">Accessibility</h3>
-              <p class="mb-2 text-gray-500">To ensure the medicines are available to those who need them the most, we allow NGOs, Hospitals and individuals to buy unused medicines from households.</p>
+              <p class="mb-2 text-gray-500">To ensure the medicines are available to those who need them the most, we allow individuals to buy unused medicines from households.</p>
             </div>
           </div>
   
@@ -112,19 +112,6 @@ also addressing the issue of affordability and accessibility of medicines for th
             <div>
               <h3 class="mb-2 text-lg font-semibold md:text-xl">Unused Medicines</h3>
               <p class="mb-2 text-gray-500">Individuals can easily donate their unused medicines on our platform</p>
-            </div>
-          </div>
-         
-          <div class="flex gap-4 md:gap-6">
-            <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
-              </svg>
-            </div>
-    
-            <div>
-              <h3 class="mb-2 text-lg font-semibold md:text-xl">Support</h3>
-              <p class="mb-2 text-gray-500">Filler text is dummy text which has no meaning however looks very similar to real text.</p>
             </div>
           </div>
         
@@ -152,7 +139,7 @@ also addressing the issue of affordability and accessibility of medicines for th
           
           <div class="flex flex-col items-center rounded-lg bg-gray-100 p-4 lg:p-8">
             <div class="mb-2 h-24 w-24 overflow-hidden rounded-full bg-gray-200 shadow-lg md:mb-4 md:h-32 md:w-32">
-              <img src="https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=256" loading="lazy" alt="Photo by Radu Florin" class="h-full w-full object-cover object-center" />
+              <img src="https://pbs.twimg.com/profile_images/1578055270747734016/1VqJrL0c_400x400.jpg" loading="lazy" alt="Photo by Radu Florin" class="h-full w-full object-cover object-center" />
             </div>
     
             <div>
@@ -269,9 +256,9 @@ also addressing the issue of affordability and accessibility of medicines for th
           <div class="col-span-full lg:col-span-2">
             <div class="mb-4 lg:-mt-2">
               <a href="/" class="inline-flex items-center gap-2 text-xl font-bold text-gray-100 md:text-2xl" aria-label="logo">
-                <svg width="95" height="94" viewBox="0 0 95 94" class="h-auto w-5 text-indigo-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                {/* <svg width="95" height="94" viewBox="0 0 95 94" class="h-auto w-5 text-indigo-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                   <path d="M96 0V47L48 94H0V47L48 0H96Z" />
-                </svg>
+                </svg> */}
     
                 MedBridge
               </a>
@@ -280,23 +267,6 @@ also addressing the issue of affordability and accessibility of medicines for th
             <p class="mb-6 text-gray-400 sm:pr-8">Accessible healthcare, anytime, anywhere</p>
     
             <div class="flex gap-4">
-              <a href="#" target="_blank" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
-                <svg class="h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
-                </svg>
-              </a>
-    
-              <a href="#" target="_blank" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
-                <svg class="h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-                </svg>
-              </a>
-    
-              <a href="#" target="_blank" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
-                <svg class="h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
-                </svg>
-              </a>
     
               <a href="#" target="_blank" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
                 <svg class="h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -305,29 +275,6 @@ also addressing the issue of affordability and accessibility of medicines for th
               </a>
             </div>
             
-          </div>
-    
- 
-          <div>
-            <div class="mb-4 font-bold uppercase tracking-widest text-gray-100">Products</div>
-    
-            <nav class="flex flex-col gap-4">
-              <div>
-                <a href="#" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">Overview</a>
-              </div>
-    
-              <div>
-                <a href="#" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">Solutions</a>
-              </div>
-    
-              <div>
-                <a href="#" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">Pricing</a>
-              </div>
-    
-              <div>
-                <a href="#" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">Customers</a>
-              </div>
-            </nav>
           </div>
        
           {/* <div>
@@ -368,13 +315,6 @@ also addressing the issue of affordability and accessibility of medicines for th
                 <a href="#" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">Documentation</a>
               </div>
     
-              <div>
-                <a href="#" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">Chat</a>
-              </div>
-    
-              <div>
-                <a href="#" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">FAQ</a>
-              </div>
             </nav>
           </div>
         
--- pages/sellerPage.js
+++ pages/sellerPage.js
@@ -15,8 +15,8 @@ export default function sellerPage() {
             <label for="username" className="block text-sm font-medium leading-6 text-gray-900">Username</label>
             <div className="mt-2">
               <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
-                <input type="text" name="username" id="username" autocomplete="username" className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="janesmith"/>
+                <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+                <input type="text" name="username" id="username" autocomplete="username" className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder=""/>
                   
               </div>
             </div>
@@ -41,7 +41,7 @@ export default function sellerPage() {
           </div>
   
           <div className="col-span-full">
-            <label for="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">Cover photo</label>
+            <label for="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">Medicine photo</label>
             <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
               <div className="text-center">
                 <svg className="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
@@ -91,9 +91,7 @@ export default function sellerPage() {
             <label for="country" className="block text-sm font-medium leading-6 text-gray-900">Country</label>
             <div className="mt-2">
               <select id="country" name="country" autocomplete="country-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                <option>United States</option>
-                <option>Canada</option>
-                <option>Mexico</option>
+                <option>India</option>
               </select>
             </div>
           </div>
@@ -128,63 +126,6 @@ export default function sellerPage() {
         </div>
       </div>
   
-      <div className="border-b border-gray-900/10 pb-12">
-        <h2 className="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
-        <p className="mt-1 text-sm leading-6 text-gray-600">We'll always let you know about important changes, but you pick what else you want to hear about.</p>
-  
-        <div className="mt-10 space-y-10">
-          <fieldset>
-            <legend className="text-sm font-semibold leading-6 text-gray-900">By Email</legend>
-            <div className="mt-6 space-y-6">
-              <div className="relative flex gap-x-3">
-                <div className="flex h-6 items-center">
-                  <input id="comments" name="comments" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
-                </div>
-                <div className="text-sm leading-6">
-                  <label for="comments" className="font-medium text-gray-900">Comments</label>
-                  <p className="text-gray-500">Get notified when someones posts a comment on a posting.</p>
-                </div>
-              </div>
-              <div className="relative flex gap-x-3">
-                <div className="flex h-6 items-center">
-                  <input id="candidates" name="candidates" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
-                </div>
-                <div className="text-sm leading-6">
-                  <label for="candidates" className="font-medium text-gray-900">Candidates</label>
-                  <p className="text-gray-500">Get notified when a candidate applies for a job.</p>
-                </div>
-              </div>
-              <div className="relative flex gap-x-3">
-                <div className="flex h-6 items-center">
-                  <input id="offers" name="offers" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
-                </div>
-                <div className="text-sm leading-6">
-                  <label for="offers" className="font-medium text-gray-900">Offers</label>
-                  <p className="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
-                </div>
-              </div>
-            </div>
-          </fieldset>
-          <fieldset>
-            <legend className="text-sm font-semibold leading-6 text-gray-900">Push Notifications</legend>
-            <p className="mt-1 text-sm leading-6 text-gray-600">These are delivered via SMS to your mobile phone.</p>
-            <div className="mt-6 space-y-6">
-              <div className="flex items-center gap-x-3">
-                <input id="push-everything" name="push-notifications" type="radio" className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
-                <label for="push-everything" className="block text-sm font-medium leading-6 text-gray-900">Everything</label>
-              </div>
-              <div className="flex items-center gap-x-3">
-                <input id="push-email" name="push-notifications" type="radio" className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
-                <label for="push-email" className="block text-sm font-medium leading-6 text-gray-900">Same as email</label>
-              </div>
-              <div className="flex items-center gap-x-3">
-                <input id="push-nothing" name="push-notifications" type="radio" className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
-                <label for="push-nothing" className="block text-sm font-medium leading-6 text-gray-900">No push notifications</label>
-              </div>
-            </div>
-          </fieldset>
-        </div>
-      </div>
     </div>
   
     <div className="mt-6 flex items-center justify-end gap-x-6">
