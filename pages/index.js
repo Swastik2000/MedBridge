3d626e437d39c2e21ea4309c5f59648282f9def8
Swastik Mishra <swastik.mishra@databricks.com>
2023-06-07 14:48:04 +0000

    redirect

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ export default function Index() {
             {/* <svg width="95" height="94" viewBox="0 0 95 94" class="h-auto w-6 text-indigo-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
               <path d="M96 0V47L48 94H0V47L48 0H96Z" />
             </svg> */}
-            
     
             MedBridge
           </a>
@@ -23,8 +22,9 @@ export default function Index() {
             <a href="#" class="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">About</a>
           </nav> */}
           
+          <Link href="/user/login">
           <a href="./sellerPage" class="hidden rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:inline-block">Sign In</a>
-    
+          </Link>
           <button type="button" class="inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-indigo-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
