937cff148ddbc3b66a720f4b46ab6fdbcc330d52
Soumava-221B <77767745+Soumava-221B@users.noreply.github.com>
2023-06-07 17:15:52 +0000

    Logo

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,9 @@ export default function Index() {
         <header class="mb-8 flex items-center justify-between py-4 md:mb-12 md:py-8 xl:mb-16">
  
           <a href="/" class="text-black-800 inline-flex items-center gap-2.5 text-2xl font-bold md:text-3xl" aria-label="logo">
-            {/* <svg width="95" height="94" viewBox="0 0 95 94" class="h-auto w-6 text-indigo-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path d="M96 0V47L48 94H0V47L48 0H96Z" />
-            </svg> */}
-    
+          <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+            <img src="https://cdn-icons-png.flaticon.com/512/9340/9340061.png" />
+            </div>
             MedBridge
           </a>
     
