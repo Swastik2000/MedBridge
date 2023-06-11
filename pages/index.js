08a777125ef13c9e73f0a5e8f01240feca78e277
Soumava-221B <77767745+Soumava-221B@users.noreply.github.com>
2023-06-11 15:35:03 +0000

    Team info

--- pages/index.js
+++ pages/index.js
@@ -158,22 +158,22 @@ also addressing the issue of accessibility of medicines for those who need them 
     
           <div class="flex flex-col items-center rounded-lg bg-gray-100 p-4 lg:p-8">
             <div class="mb-2 h-24 w-24 overflow-hidden rounded-full bg-gray-200 shadow-lg md:mb-4 md:h-32 md:w-32">
-              <img src="https://images.unsplash.com/photo-1532073150508-0c1df022bdd1?auto=format&q=75&fit=crop&w=256" loading="lazy" alt="Photo by christian ferrer" class="h-full w-full object-cover object-center" />
+              <img src="" loading="lazy" alt="Photo by christian ferrer" class="h-full w-full object-cover object-center" />
             </div>
     
             <div>
-              <div class="text-center font-bold text-indigo-500 md:text-lg">Kate Berg</div>
-              <p class="mb-3 text-center text-sm text-gray-500 md:mb-4 md:text-base">CFO</p>
+              <div class="text-center font-bold text-indigo-500 md:text-lg">Isha Sinha</div>
+              <p class="mb-3 text-center text-sm text-gray-500 md:mb-4 md:text-base">Web Design and Content Writter</p>
     
               <div class="flex justify-center">
                 <div class="flex gap-4">
-                  <a href="#" target="_blank" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
+                  <a href="https://www.linkedin.com/in/ishasinha28/" target="https://www.linkedin.com/in/ishasinha28/" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
                     <svg class="h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                       <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
                     </svg>
                   </a>
     
-                  <a href="#" target="_blank" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
+                  <a href="https://twitter.com/IshaSinha28" target="https://twitter.com/IshaSinha28" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
                     <svg class="h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                       <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
                     </svg>
@@ -185,22 +185,22 @@ also addressing the issue of accessibility of medicines for those who need them 
        
           <div class="flex flex-col items-center rounded-lg bg-gray-100 p-4 lg:p-8">
             <div class="mb-2 h-24 w-24 overflow-hidden rounded-full bg-gray-200 shadow-lg md:mb-4 md:h-32 md:w-32">
-              <img src="https://images.unsplash.com/photo-1463453091185-61582044d556?auto=format&q=75&fit=crop&w=256" loading="lazy" alt="Photo by Ayo Ogunseinde" class="h-full w-full object-cover object-center" />
+              <img src="https://media.licdn.com/dms/image/C4D03AQGJRR2dpXNCdw/profile-displayphoto-shrink_200_200/0/1635673712353?e=1692230400&v=beta&t=CSTXq2DHvu5r_PC3-CoCotUNJDHK4EosCfuNqAUaFL4" loading="lazy" alt="Photo by Ayo Ogunseinde" class="h-full w-full object-cover object-center" />
             </div>
     
             <div>
-              <div class="text-center font-bold text-indigo-500 md:text-lg">Greg Jackson</div>
-              <p class="mb-3 text-center text-sm text-gray-500 md:mb-4 md:text-base">CTO</p>
+              <div class="text-center font-bold text-indigo-500 md:text-lg">Swastik Mishra</div>
+              <p class="mb-3 text-center text-sm text-gray-500 md:mb-4 md:text-base">Backend Developer</p>
     
               <div class="flex justify-center">
                 <div class="flex gap-4">
-                  <a href="#" target="_blank" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
+                  <a href="https://www.linkedin.com/in/swastikmishra2000/" target="https://www.linkedin.com/in/swastikmishra2000/" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
                     <svg class="h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                       <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
                     </svg>
                   </a>
     
-                  <a href="#" target="_blank" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
+                  <a href="https://twitter.com/swastikm2000" target="https://twitter.com/swastikm2000" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
                     <svg class="h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                       <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
                     </svg>
@@ -212,16 +212,16 @@ also addressing the issue of accessibility of medicines for those who need them 
       
           <div class="flex flex-col items-center rounded-lg bg-gray-100 p-4 lg:p-8">
             <div class="mb-2 h-24 w-24 overflow-hidden rounded-full bg-gray-200 shadow-lg md:mb-4 md:h-32 md:w-32">
-              <img src="https://images.unsplash.com/photo-1529068755536-a5ade0dcb4e8?auto=format&q=75&fit=crop&w=256" loading="lazy" alt="Photo by Midas Hofstra" class="h-full w-full object-cover object-center" />
+              <img src="https://media.licdn.com/dms/image/D5603AQHD6rhcywiRig/profile-displayphoto-shrink_200_200/0/1673070796711?e=1692230400&v=beta&t=yza8oT2O_jLMmmGH94Sk-s2vOb3Sg-HjpNNHfI6ea5o" loading="lazy" alt="Photo by Midas Hofstra" class="h-full w-full object-cover object-center" />
             </div>
     
             <div>
-              <div class="text-center font-bold text-indigo-500 md:text-lg">Robert Greyson</div>
-              <p class="mb-3 text-center text-sm text-gray-500 md:mb-4 md:text-base">Creative Director</p>
+              <div class="text-center font-bold text-indigo-500 md:text-lg">Ayush Agrawal</div>
+              <p class="mb-3 text-center text-sm text-gray-500 md:mb-4 md:text-base">Database</p>
     
               <div class="flex justify-center">
                 <div class="flex gap-4">
-                  <a href="#" target="_blank" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
+                  <a href="https://www.linkedin.com/in/ayush-agrawal-032a6a189/" target="https://www.linkedin.com/in/ayush-agrawal-032a6a189/" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
                     <svg class="h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                       <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
                     </svg>
